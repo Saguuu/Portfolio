5058b77b9de291a6a9acde4571892ce439bd7f43
Saguuu <hjaros1997@gmail.com>
2022-05-03 23:46:00 -0500

    Finish contact section and added react-scroll

--- portfolio-react-app/src/components/Navbar.jsx
+++ portfolio-react-app/src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import { FaBars, FaTimes, FaGithub, FaLinkedin} from "react-icons/fa";
 import { HiOutlineMail } from "react-icons/hi"
 import { BsFillPersonLinesFill } from "react-icons/bs";
 import Logo from "../assets/logo.png";
+import { Link } from 'react-scroll'
 
 const Navbar = () => {
 
@@ -17,11 +18,31 @@ const Navbar = () => {
         </div>
 
         <ul className="hidden md:flex">
-            <li>Home</li>
-            <li>About</li>
-            <li>Skills</li>
-            <li>Projects</li>
-            <li>Contact</li>
+            <li>
+                <Link activeClass="active" to="home" smooth={true} duration={500}>
+                    Home
+                </Link>
+            </li>
+            <li>
+                <Link activeClass="active" to="about" smooth={true} duration={500}>
+                    About
+                </Link>
+            </li>
+            <li>
+                <Link activeClass="active" to="skills" smooth={true} duration={500}>
+                    Skills
+                </Link>
+            </li>
+            <li>
+                <Link activeClass="active" to="work" smooth={true} duration={500}>
+                    Projects
+                </Link>
+            </li>
+            <li>
+                <Link activeClass="active" to="contact" smooth={true} duration={500}>
+                    Contact
+                </Link>
+            </li>
         </ul>
        
         <div onClick={handleClick} className="md:hidden z-10">
@@ -29,11 +50,21 @@ const Navbar = () => {
         </div>
 
         <ul className={!nav ? "hidden" : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"}>
-            <li className="py-6 text-4xl">Home</li>
-            <li className="py-6 text-4xl">About</li>
-            <li className="py-6 text-4xl">Skills</li>
-            <li className="py-6 text-4xl">Projects</li>
-            <li className="py-6 text-4xl">Contact</li>
+            <li onClick={handleClick} className="py-6 text-4xl"><Link onClick={handleClick} activeClass="active" to="home" smooth={true} duration={500}>
+                    Home
+                </Link></li>
+            <li className="py-6 text-4xl"><Link onClick={handleClick} activeClass="active" to="about" smooth={true} duration={500}>
+                    About
+                </Link></li>
+            <li className="py-6 text-4xl"><Link onClick={handleClick} activeClass="active" to="skills" smooth={true} duration={500}>
+                    Skills
+                </Link></li>
+            <li className="py-6 text-4xl"><Link onClick={handleClick} activeClass="active" to="work" smooth={true} duration={500}>
+                    Projects
+                </Link></li>
+            <li className="py-6 text-4xl"><Link onClick={handleClick} activeClass="active" to="contact" smooth={true} duration={500}>
+                    Contact
+                </Link></li>
         </ul>
 
         <div className="hidden lg:flex fixed flex-col top-[35%] left-0">
