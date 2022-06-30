cb23255bd4cfa28087ec68d7a301d646d75fb470
Saguuu <hjaros1997@gmail.com>
2022-06-30 16:43:50 -0500

    re

--- portfolio-react-app/src/components/Contact.jsx
+++ portfolio-react-app/src/components/Contact.jsx
@@ -8,7 +8,7 @@ const Contact = () => {
                 <p className="text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300">
                     Contact
                 </p>
-                <p className="text-gray-300 py-4">// Submit form below or send me an email at KobiLTrem@email.com</p>
+                <p className="text-gray-300 py-4">// Submit form below or send me an email at KobiLTrem@gmail.com</p>
             </div>
             <input className="bg-[#ccd6f6] p-2" type="text" placeholder="Name" name="name" />
             <input className="my-4 p-2 bg-[#ccd6f6]" type="text" placeholder="Email" name="email" />
